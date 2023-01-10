37571b44a029bb651120ac627db4dab54f7e87b2
Claud Robinson <crobinson4@west.com>
2023-01-10 13:29:30 -0600

    Merge and Quick sort.

--- src/test/java/datastructures/test/SortingTest.java
+++ src/test/java/datastructures/test/SortingTest.java
@@ -1,8 +1,6 @@
 package datastructures.test;
 
-import my.datastructures.BubbleSort;
-import my.datastructures.InsertionSort;
-import my.datastructures.SelectionSort;
+import my.datastructures.*;
 
 import java.util.Arrays;
 
@@ -12,6 +10,10 @@ public class SortingTest {
         testBubbleSort();
         testSelectionSort();
         testInsertionSort();
+        testMerge();
+        testMergeSort();
+        testPivot();
+        testQuickSort();
     }
 
     private static void testBubbleSort() {
@@ -49,5 +51,76 @@ public class SortingTest {
         System.out.println("Sorted Array:");
         System.out.println(Arrays.toString(myArray));
     }
+
+    public static void testMerge(){
+        System.out.println("/n ----- Test: Merge -----");
+        int[] array1 = {1,3,7,8};
+        int[] array2 = {2,4,5,6};
+
+        System.out.println( Arrays.toString( MergeSort.merge(array1, array2) ) );
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            [1, 2, 3, 4, 5, 6, 7, 8]
+
+         */
+    }
+
+    public static void testMergeSort(){
+        System.out.println("/n ----- Test: Merge SORT -----");
+        int[] originalArray = {3,1,4,2};
+
+        int [] sortedArray = MergeSort.mergeSort(originalArray);
+
+        System.out.println( "\nOriginal Array: " + Arrays.toString( originalArray ) );
+
+        System.out.println( "\nSorted Array: " + Arrays.toString( sortedArray ) );
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Original Array: [3, 1, 4, 2]
+
+            Sorted Array: [1, 2, 3, 4]
+
+         */
+    }
+
+    public static void testPivot(){
+        System.out.println("/n----- Test: Pivot -----");
+        int[] myArray = {4,6,1,7,3,2,5};
+
+        int returnedIndex = QuickSort.pivot(myArray, 0, 6);
+
+        System.out.println( "\nReturned Index: " + returnedIndex);
+
+        System.out.println( Arrays.toString( myArray ) );
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Returned Index: 3
+            [2, 1, 3, 4, 6, 7, 5]
+
+         */
+    }
+
+    public static void testQuickSort(){
+        System.out.println("---- Testing Quick Sort ----");
+        int[] myArray = {4,6,1,7,3,2,5};
+
+        QuickSort.quickSort(myArray);
+
+        System.out.println( Arrays.toString( myArray ) );
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            [1, 2, 3, 4, 5, 6, 7]
+
+         */
+    }
+
 
 }
